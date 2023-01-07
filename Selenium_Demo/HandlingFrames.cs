a871353c876250e2b721b1f8a025393fae77e660
Ananda Gummadilli <v-anandag@microsoft.com>
2023-01-07 08:50:37 +0530

    Added HandlingFrames class

---
+++ Selenium_Demo/HandlingFrames.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_Demo
+{
+    public class HandlingFrames
+    {
+        public IWebDriver dr;
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("I am from setup method");
+            dr = new ChromeDriver(@"C:\Users\v-anandag\Desktop");
+
+        }
+        [Test]
+        public void VerifyHandlingFrames()
+        {
+            dr.Navigate().GoToUrl("https://demo.guru99.com/test/guru99home/");
+            IWebElement elementFrame = dr.FindElement(By.XPath("//iframe[@name='a077aa5e']"));
+            dr.SwitchTo().Frame(elementFrame);
+            IWebElement imgJmeter = dr.FindElement(By.XPath("//a[@href='http://www.guru99.com/live-selenium-project.html']"));
+            imgJmeter.Click();
+        }
+    }
+}
