fb87457f85da3c3953e7371628d23eaa374e5c67
Ananda Gummadilli <v-anandag@microsoft.com>
2023-04-16 13:08:15 +0530

    Added test cases to AxisMFpage.cs

---
+++ Selenium_Demo/Pages/AxisMfPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Selenium_Demo.Pages
+{
+    public class AxisMfPage
+    {
+        IWebDriver dr;
+        public AxisMfPage(IWebDriver driver)
+        {
+            dr = driver;
+        }
+
+        public void NavigatetoAxisMF()
+        {
+            dr.Navigate().GoToUrl("https://axismf.com");
+        }
+    }
+}
