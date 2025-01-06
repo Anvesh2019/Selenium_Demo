fdae1c4586fb3959134ffdfa78d3d00cc2d18d7f
Ananda Babu Gummadilli <Anand.Gummadilli@neudesic.com>
2025-01-06 08:31:08 +0530

    Added AddNumbers test case

---
+++ Selenium_Demo/TestCases/clsNoDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Selenium_Demo.TestCases
+{
+    public class clsNoDriver
+    {
+        [Test]
+        public void AddNumbers()
+        {
+            int x = 30;
+            int y = 50;
+            int z = x + y;
+            Console.WriteLine("Sum is:" + z);
+        }
+    }
+}
