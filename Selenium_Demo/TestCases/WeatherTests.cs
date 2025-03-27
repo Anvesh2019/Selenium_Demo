d80728cf6246f32d1b81311c7727fcd281274f95
Ananda Babu Gummadilli <Anand.Gummadilli@neudesic.com>
2025-03-27 14:53:13 +0530

    Added new test cases file WeaterTests.cs

---
+++ Selenium_Demo/TestCases/WeatherTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Selenium_Demo.TestCases
+{
+    public class WeatherTests
+    {
+        [Test]
+        public void GetWeaterByZip()
+        {
+
+        }
+    }
+}
