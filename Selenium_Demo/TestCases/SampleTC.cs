3230a083788cf3bcb85214cbf559eb06cdc97140
Anand <anandg99@yahoo.com>
2025-09-22 13:29:57 +0530

    modifided sample test case

---
+++ Selenium_Demo/TestCases/SampleTC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Selenium_Demo.TestCases
+{
+    public class SampleTC
+    {
+        [Test]
+        [Category("Mamatha_TC")]
+        public void Mamatha_TC1()
+        {
+
+            Console.WriteLine("sample test case1");
+            Console.WriteLine("sample code");
+        }
+    }
+}
